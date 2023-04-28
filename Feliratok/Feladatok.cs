881961b101a95bb70833a826c105d0a1ad646226
Benedek Schlesser <schlesser.benedek@petrik.hu>
2023-04-28 13:01:55 +0200

    Add project files.

---
+++ Feliratok/Feladatok.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Feliratok
+{
+    internal class Feladatok
+    {
+        public List<IdozitettFelirat> lista;
+        public Feladatok()
+        {
+            lista=new List<IdozitettFelirat> ();
+        }
+        public bool Beolvas()
+        {
+            StreamReader sr = new StreamReader("feliratok.txt");
+            while(!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string line2=sr.ReadLine();
+                lista.Add(new IdozitettFelirat(line, line2));
+            }
+            if(lista.Count > 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        public string szavakSzama()
+        {
+            int a = 0;
+            string[] b;
+            string c="";
+            foreach(var item in lista)
+            {
+                b = item.Felirat.Split(' ');
+                if(b.Length > a)
+                {
+                    a = b.Length;
+                    c = Convert.ToString(item);
+                }
+            }
+            return c;
+        }
+
+    }
+}
---
+++ Feliratok/IdozitettFelirat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feliratok
+{
+    internal class IdozitettFelirat
+    {
+        private string idozites;
+        private string felirat;
+
+        public IdozitettFelirat(string idozites, string felirat)
+        {
+            this.idozites = idozites;
+            this.felirat = felirat;
+        }
+
+        public string Idozites { get => idozites; set => idozites = value; }
+        public string Felirat { get => felirat; set => felirat = value; }
+        public override string ToString()
+        {
+            return $"{this.idozites} --- {this.felirat}";
+        }
+    }
+}
